23241d2988a493f5b4c3f2c921cf2043710d2ab9
shekinah <nyamburamary1212@gmail.com>
2023-10-24 12:03:36 +0300

    complete hook components basics

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+  return (
+    <div>
+      <p>Naturally, I agree with this article.</p>
+      <p>This is my comment!</p>
+    </div>
+    )
 }
 
 export default Comment;
